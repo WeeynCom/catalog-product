e49a7b267c6373a57b13a7eff0393a773a0e8b82
Fahrettin Aksoy <aksoy@fahrettinaksoy.com.tr>
2023-03-08 13:39:28 +0300

    Create A Module

---
+++ src/Providers/ModuleServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Vendor\MyModule\Providers;
+
+use Konekt\Concord\BaseModuleServiceProvider;
+
+class ModuleServiceProvider extends BaseModuleServiceProvider
+{
+	
+}
---
+++ src/resources/manifest.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+	'name' => 'Weeyn Product Module',
+	'version' => '1.0.0'
+];
