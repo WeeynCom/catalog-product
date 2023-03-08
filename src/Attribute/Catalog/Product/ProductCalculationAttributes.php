ddcde8c66965a078516f78fac6b6628ecd0fc4f9
Fahrettin Aksoy <aksoy@fahrettinaksoy.com.tr>
2023-03-08 16:28:22 +0300

    aaa

--- src/Attribute/Catalog/Product/ProductCalculationAttributes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Attribute\Catalog\Product;
-
-trait ProductCalculationAttributes
-{
-	
-}
--- src/resources/manifest.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-	'name' => 'Weeyn Product Module',
-	'version' => '1.0.0'
-];
